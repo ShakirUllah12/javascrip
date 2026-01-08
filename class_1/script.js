5700aab9e99386a22d6aff2533125f313f6e85ef
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-01-09 02:02:21 +0500

    first class done

---
+++ class_1/script.js
@@ -0,0 +1,21 @@
+
+// how we can show data in browser 
+// console.log("My name is shakir ullah");
+// var description="my name is shakir ulah i am froom pakistan ";
+// console.log(description);
+
+// variable decleration 
+// var a;//decleration
+// a=10;//initilization
+// var a=10;//inishilize//and decleare
+// console.log(typeof(a));
+
+// var name="shakir";
+// name="gull khan";
+// console.log(name);
+// var name="shakir";
+// console.log(name);
+// var age=21;
+// console.log(age);
+// var gender="male";
+// console.log(gender);
