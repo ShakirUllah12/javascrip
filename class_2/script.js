97c5a949968f2b6a6bd50bb24d252cba4ad4795a
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-01-09 02:25:19 +0500

    done

---
+++ class_2/script.js
@@ -0,0 +1,16 @@
+// let keywords in javascrip
+// let a=12;
+// a=30;
+// console.log(a);
+
+// const variable in js 
+// const userName="shakir";
+// userName="ahammad";
+// console.log(userName);
+
+// let name="shakir";
+// let message="my friend "+name;
+// console.log(message);
+let name="shakir";
+let message=`my friend name is ${name}`;
+console.log(message);
