13d9ffb716ee62e6fa684a958afed3f9aaf27366
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-01-09 10:43:39 +0500

    second class done

--- class_2/script.js
+++ class_2/script.js
@@ -11,6 +11,30 @@
 // let name="shakir";
 // let message="my friend "+name;
 // console.log(message);
-let name="shakir";
-let message=`my friend name is ${name}`;
-console.log(message);
+// let name="shakir";
+// let message=`my friend name is ${name}`;
+// console.log(message);
+
+// let name="shakkir";
+// let skills="Mern stack ";
+// let year=2003;
+// let bio=`my name is ${name}my skills is ${skills}in passing year ${year}`;
+// console.log(bio);
+// Data types in javascrip 
+// 1)Number 
+// let num1=10;
+// let num2=20;
+// console.log(typeof (num1+num2));
+// 2)String 
+// let name="shakir";
+// let fName="said Nabi khan";
+// console.log(typeof"name+fName");
+
+// let nightMode=true;
+// console.log(typeof(nightMode));
+
+// let num=BigInt(12345678901234567890);
+// console.log(typeof(num));
+
+let name=null;
+console.log(typeof(name));
